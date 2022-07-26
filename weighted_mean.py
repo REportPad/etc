14cb23b151790cf77ef96011375dca80a79a37ef
jehyun <ggurggur123@naver.com>
2022-07-27 05:57:58 +0900

    Update weighted_mean.py

--- weighted_mean.py
+++ weighted_mean.py
@@ -2,12 +2,9 @@ def weighted_mean(arr):
     n = len(arr)
     #print(n)
     a1 = 2/(n*(n+1))
-    #print(a1)
-    d = 2*(1-n*a1)/(n*(n-1))
-    #print(d)
     x = 0
     for i in range(1,n+1):
-        x = x + (arr[i-1]*(a1))
-        a1 = a1 + d
+        x = x + (i*arr[i-1])
         
+    x = a1*x
     return x
